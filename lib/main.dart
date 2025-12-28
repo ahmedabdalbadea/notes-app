9fdcf3e04a3926b862b59720ec8e6818be0737e4
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2025-12-28 20:36:06 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/pages/home_page.dart';
+
+void main(){
+  runApp(NotesApp());
+}
+
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
