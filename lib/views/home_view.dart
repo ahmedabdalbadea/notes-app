2c7c263f36930e81c72a3a30838522dc46feec28
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2025-12-29 21:53:21 +0200

    add floating action button

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class HomeView extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NotesViewBody(),
+      body: const NotesViewBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
-
-
-
