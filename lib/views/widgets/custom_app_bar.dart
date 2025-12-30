640315385d82ab86ccc975fa55d35ac160c29412
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2025-12-30 21:01:18 +0200

    ui done ✔️

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+
 import 'package:notes_app/views/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({
-    super.key,
-  });
-
+    Key? key,
+    required this.title,
+    required this.icon,
+  }) : super(key: key);
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text("Notes", style: TextStyle(
+        Text(title, style: TextStyle(
           fontSize: 28,
         ),),
-        CustomSearchIcon()
+        CustomSearchIcon(icon: icon,)
       ],
     );
   }
--- lib/views/widgets/custom_search_icon.dart
+++ lib/views/widgets/custom_search_icon.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 class CustomSearchIcon extends StatelessWidget {
   @override
+  final IconData icon;
+
+  const CustomSearchIcon({super.key, required this.icon});
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         color: Colors.white.withValues(alpha: 0.05),
         borderRadius: BorderRadius.circular(18),
       ),
-      child: IconButton(onPressed: () {}, icon: Icon(Icons.search, size: 28)),
+      child: IconButton(onPressed: () {}, icon: Icon(icon, size: 28)),
     );
   }
 }
