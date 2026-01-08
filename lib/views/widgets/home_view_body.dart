d2fbead7e7e99169016e2689d5a8c3f138376b47
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-01-08 21:48:17 +0200

    add note item

--- lib/views/widgets/home_view_body.dart
+++ lib/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/note_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -12,6 +13,7 @@ class HomeViewBody extends StatelessWidget {
         children: [
           const SizedBox(height: 50),
           const CustomAppBar(title: "Notes", icon: Icons.search),
+          const NoteItem(),
         ],
       ),
     );
---
+++ lib/views/widgets/note_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, left: 24, right: 16, bottom: 24),
+      decoration: BoxDecoration(
+        color: Colors.amberAccent,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children:  [
+          ListTile(
+            contentPadding: EdgeInsets.zero,
+            title: const Text(
+              "Flutter Tips",
+              style: TextStyle(fontSize: 24, color: Colors.black),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12.0),
+              child: Text(
+                "Build your career with Tharwat Samy",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black.withValues(alpha: 0.4),
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              padding: EdgeInsets.only(bottom: 24),
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.trash, size: 24, color: Colors.black),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Text(
+              "May 21,2022",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black.withValues(alpha: 0.4),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
