6488693ba6ef72fd4effaaa63f8954eaf09f2157
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-01-08 20:29:56 +0200

    change theme and create home view

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/home_view.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: HomeView(),
+    );
+  }
+}
