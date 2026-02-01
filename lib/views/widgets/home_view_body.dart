ea63ab5a309f129f7ab4fea2cbbec5b53d4719a0
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-02-01 21:16:42 +0200

    create note item

--- lib/views/widgets/home_view_body.dart
+++ lib/views/widgets/home_view_body.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/note_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 50),
-        CustomAppBar(
-          title: "Notes",
-          icon: Icon(Icons.search),
-          onPressed: () {},
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          CustomAppBar(
+            title: "Notes",
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          SizedBox(height: 16),
+          NoteItem(
+            title: "Flutter Tips",
+            subTitle: "Build Your Career in Flutter",
+            date: "Jan 31, 2026",
+            color: Colors.amber,
+          ),
+        ],
+      ),
     );
   }
 }
