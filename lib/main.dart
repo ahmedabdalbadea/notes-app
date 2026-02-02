0130a38bee86fbe9993f4fff57d093718808e15d
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-02-02 13:27:21 +0200

    init hive and create nodal note and adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/home_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Hive.registerAdapter(NoteModelAdapter());
+  Hive.openBox<NoteModel>(kNotesBox);
+  await Hive.initFlutter();
   runApp(NotesApp());
 }
 
