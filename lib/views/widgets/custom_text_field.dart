88cee04a5fb309de090c5103035fa9f3c4797c4a
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-02-02 13:56:50 +0200

    create addNote cubit and state and function for it

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({Key? key, required this.title, this.maxLines = 1})
-    : super(key: key);
+  const CustomTextField({
+    Key? key,
+    required this.title,
+    this.onSaved,
+    this.maxLines = 1,
+  }) : super(key: key);
   final String title;
   final int maxLines;
+  final Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onSaved: onSaved,
       validator: (value) {
         if (value?.isEmpty ?? true) {
           return "this filed is required";
--- lib/views/widgets/modal_bottom_sheet_form.dart
+++ lib/views/widgets/modal_bottom_sheet_form.dart
@@ -2,28 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_elevated_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
-class ModalBottomSheetForm extends StatelessWidget {
+class ModalBottomSheetForm extends StatefulWidget {
   const ModalBottomSheetForm({super.key});
+
+  @override
+  State<ModalBottomSheetForm> createState() => _ModalBottomSheetFormState();
+}
+
+class _ModalBottomSheetFormState extends State<ModalBottomSheetForm> {
+  String? title, subTitle;
+
+  GlobalKey<FormState> fromKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
   @override
   Widget build(BuildContext context) {
-  GlobalKey<FormState> fromKey = GlobalKey();
-  
     return Form(
       key: fromKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
         children: [
-          CustomTextField(title: "Title"),
+          CustomTextField(
+            title: "Title",
+            onSaved: (value) {
+              title = value;
+            },
+          ),
           SizedBox(height: 16),
-          CustomTextField(title: "Content", maxLines: 5),
+          CustomTextField(
+            title: "Content",
+            onSaved: (value) {
+              subTitle = value;
+            },
+            maxLines: 5,
+          ),
           SizedBox(height: 16),
-          CustomElevatedButton(title: "Add", onPressed: () {
-            if(fromKey.currentState!.validate()){
-              fromKey.currentState!.save();
-            }
-            else {
-
-            }
-          }),
+          CustomElevatedButton(
+            title: "Add",
+            onPressed: () {
+              if (fromKey.currentState!.validate()) {
+                fromKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
           SizedBox(height: 16),
         ],
       ),
